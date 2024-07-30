dfc65155774afd12977497741912e193278e0be0
Klysheee <153086918+Klysheee@users.noreply.github.com>
2024-07-30 14:03:33 +0300

    calculator

--- lan.js
+++ lan.js
@@ -2,7 +2,21 @@ let a = Number(prompt());
 let b = Number(prompt());
 let c = prompt();
 if(a , b , c){
-     if(num2 === "-"){
-        result = (a - b);
-     }
-}
+    if(c == '+'){
+        result = a + b;
+    }
+    else if(c == '-'){
+        result = a - b;
+    }
+    if(c == '%'){
+        result = a/b;
+    }
+    if(c == '*'){
+        result = a * b;
+    }
+    else{
+        console.log("некорректный ввод");
+    }
+    
+};
+console.log(result);
